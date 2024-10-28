3a2c8e4fad59b644363c22dd2fe1483ddb5659f2
Vu Duc Toan <you@example.com>
2024-10-28 17:18:57 +0700

    feat: Finish listing support app feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+puts "Start seeding"
+App.create(name: 'Datadog')
+App.create(name: 'Sendgrid')
+puts "Seeding done"
