a7b09909e9edbb31ac61b27a7559471bec7113b5
Vu Duc Toan <you@example.com>
2024-10-30 10:06:09 +0700

    refactor: update routes and Apps resource test

--- spec/routing/apps_routing_spec.rb
+++ spec/routing/apps_routing_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe AppsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/apps").to route_to("apps#index")
+      expect(get: "/orgs/1/apps").to route_to("apps#index", org_id: '1')
     end
   end
 end
